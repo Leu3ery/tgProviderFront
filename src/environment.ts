1c2b87d49748e984c8f43fd1a4807de06a376b21
leuzery <valentyn.hvozdenko@htlstp.at>
2025-12-13 01:30:45 +0100

    fix for env

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 export const config = {
     basicURL: 'https://tgprovider.leuzery.live/api',
     botURL: 'https://t.me/yourtgprovider_bot',
-    manifestURL: 'https://tgprovider.leuzery.live/public/tonconnect-manifest.json',
+    manifestURL: 'https://tgprovider.leuzery.live/api/public/tonconnect-manifest.json',
     tonAddress: 'UQBbhd0rMrKrt4uNkSY2YI8elK-EPfVREaZ5RBYupZMlfw-b',
     koefizzient : 0.10
 
